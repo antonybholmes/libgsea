314888e6aa4bb15958fd7d51b1d5675f320a921e
Antony Holmes <antony.b.holmes@gmail.com>
2019-01-25 13:33:12 -0500

    Updates and bug fixes.

---
+++ gsea_plot.sh
@@ -0,0 +1,33 @@
+/ifs/home/cancer/Lab_RDF/Personal/Antony/gsea/gsea_plot/gsea_plot.sh \
+/ifs/home/cancer/Lab_RDF/Public/Katia/from_ANTONY/RNA_seq/mouse/RK3035_RK3050_20171208/dup/gsea/single_gene_sets_20180118/Victora_lz_2012/wt_vs_d83vko.Gsea.1516303365709/
+
+# Victora_DZ_2012 in mouse MEF2B-WT (RK3025; RK3026; RK3027) vs MEF2B-KO (RK3031; RK3032; RK3033; RK3034)
+/ifs/home/cancer/Lab_RDF/Personal/Antony/gsea/gsea_plot/gsea_plot.sh \
+/ifs/home/cancer/Lab_RDF/Public/Katia/from_ANTONY/RNA_seq/mouse/RK3025_RK3034_MEF2B_20170915/gsea/dup/20180118/single_lists/Victora_dz_2012/GC_WT_vs_KO.Gsea.1516301075385/
+
+# Victora_DZ_2012 in mouse MEF2B-WT (RK3025; RK3026; RK3027) vs MEF2B-KO (RK3031; RK3032; RK3033; RK3034)
+/ifs/home/cancer/Lab_RDF/Personal/Antony/gsea/gsea_plot/gsea_plot.sh \
+/ifs/home/cancer/Lab_RDF/Public/Katia/from_ANTONY/RNA_seq/mouse/RK3025_RK3034_MEF2B_20170915/gsea/dup/20180118/single_lists/Victora_lz_2012/GC_WT_vs_KO.Gsea.1516301253442/
+
+
+/ifs/home/cancer/Lab_RDF/Personal/Antony/gsea/gsea_plot/gsea_plot.sh \
+/ifs/home/cancer/Lab_RDF/Public/Katia/from_ANTONY/RNA_seq/mouse/RK3035_RK3050_20171208/dup/gsea/single_gene_sets_20180118/Victora_dz_2012/wt_vs_d83vko.Gsea.1516302964433/
+
+
+/ifs/home/cancer/Lab_RDF/Personal/Antony/gsea/gsea_plot/gsea_plot.sh \
+/ifs/home/cancer/Lab_RDF/Public/Katia/from_ANTONY/RNA_seq/mouse/RK3035_RK3050_20171208/dup/gsea/single_gene_sets_20180118/Bnd_GC_Up_WTvsKO_Up_141/wt_vs_d83vko.Gsea.1516302366615/
+
+/ifs/home/cancer/Lab_RDF/Personal/Antony/gsea/gsea_plot/gsea_plot.sh \
+/ifs/home/cancer/Lab_RDF/Public/Katia/from_ANTONY/RNA_seq/mouse/RK3035_RK3050_20171208/dup/gsea/single_gene_sets_20180118/Bnd_GC_Dwn_WtvsKO_Dwn_109/wt_vs_d83vko.Gsea.1516302268042/
+
+/ifs/home/cancer/Lab_RDF/Personal/Antony/gsea/gsea_plot/gsea_plot.sh \
+/ifs/home/cancer/Lab_RDF/Public/Katia/from_ANTONY/RNA_seq/mouse/RK3035_RK3050_20171208/dup/gsea/single_gene_sets_20180118/Bnd_GC_Up_WTvsKO_Up_141/wt_vs_d83v.Gsea.1516302358395/
+
+/ifs/home/cancer/Lab_RDF/Personal/Antony/gsea/gsea_plot/gsea_plot.sh \
+/ifs/home/cancer/Lab_RDF/Public/Katia/from_ANTONY/RNA_seq/mouse/RK3035_RK3050_20171208/dup/gsea/single_gene_sets_20180118/Bnd_GC_Dwn_WtvsKO_Dwn_109/wt_vs_d83v.Gsea.1516302259999/
+
+/ifs/home/cancer/Lab_RDF/Personal/Antony/gsea/gsea_plot/gsea_plot.sh \
+/ifs/home/cancer/Lab_RDF/Public/Katia/from_ANTONY/RNA_seq/mouse/RK3035_RK3050_20171208/dup/gsea/single_gene_sets_20180118/Victora_lz_2012/wt_vs_d83v.Gsea.1516303356954/
+
+/ifs/home/cancer/Lab_RDF/Personal/Antony/gsea/gsea_plot/gsea_plot.sh \
+/ifs/home/cancer/Lab_RDF/Public/Katia/from_ANTONY/RNA_seq/mouse/RK3035_RK3050_20171208/dup/gsea/single_gene_sets_20180118/Victora_dz_2012/wt_vs_d83v.Gsea.1516302956001/
