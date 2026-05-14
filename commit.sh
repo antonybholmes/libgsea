723e5cc37ae1ff2033069f8bddfdf81620062230
Antony Holmes <antony@antonyholmes.dev>
2026-05-14 09:29:34 -0400

    fix:

--- commit.sh
+++ commit.sh
@@ -1,3 +1,17 @@
-git add -A
-git commit -m 'Updates and bug fixes.'
-git push -u origin main
+msg=$1
+type=$2
+
+if [[ -z "${msg}" ]]
+then
+	mgs="fix: Bug fixes and updates."
+fi
+
+if [[ -z "${type}" ]]
+then
+	type="fix"
+fi
+
+# commit
+git add -A .
+git commit -m "${type}: ${msg}"
+git push -u origin dev
